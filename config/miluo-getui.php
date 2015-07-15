2457410c024be5c250976867ef178188f646040a
mac <346787367@qq.com>
2015-07-15 22:24:45 +0800

    getui-laravel5 个推sdk集成laravel5

---
+++ config/miluo-getui.php
@@ -0,0 +1,11 @@
+<?php
+
+return [
+    'APPKEY' => '<your_appkey>',
+    'APPID' => '<your_appid>',
+    'MASTERSECRET' => '<your_mastersecret>',
+    'HOST' => '<your_host>',
+    'CID' => '<your_cid>',
+    'DEVICETOKEN' => '<your_devicetoken>',
+    'Alias' => '<you_alias>'
+];
